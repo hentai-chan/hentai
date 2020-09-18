ebba25fd2354d2799c19dde897e5eb2ea608e13a
hentaichan <70948244+hentai-chan@users.noreply.github.com>
2020-09-18 14:59:45 +0200

    Adds static method counterparts.
    An Hentai object uses requires an API call to get instantiated. All static
    methods, however, respond with raw dictionaries because the constructor
    can not be overloaded (it only accepts optional parameters at the moment).
    
    To make sense of methods like `search_all_by_query`, pretty much all properties
    have a static `get_*` counterpart that takes a dictionary response object as
    input. For the sake of argument, let's say a query search return 123 pages.
    Then `search_all_by_query` makes 123+1 requests in order to fetch all results.
    (The first one determines the range, e.g. 123). For instance, to print all titles
    one would write
    
    ```python
    from hentai import Hentai, Format
    
    for page in Hentai.search_all_by_query('tag:loli'):
            for doujin in page:
                print(Hentai.get_title(doujin, Format.Pretty))
    ```
    
    This is a much faster solution than what other nhentai library writers
    have accomplished so far. It would be nice if a payload could be constructed
    that reduces the number of calls further. More investigations into this matter
    are needed.

--- hentai/hentai.py
+++ hentai/hentai.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum, unique
-from typing import List, Tuple
+from typing import Iterator, List, Tuple
 from urllib.parse import urljoin, urlparse
 
 import requests
@@ -21,11 +21,13 @@ class Tag:
     url: str
     count: int
 
+
 @unique
 class Format(Enum):
     English = 'english'
     Japanese = 'japanese'
     Pretty = 'pretty'
+
 
 @unique
 class Extension(Enum):
@@ -37,17 +39,18 @@ class Extension(Enum):
     def convert(cls, key: str) -> str:
         return cls(key).name.lower()
 
+
 class RequestHandler(object):
-    TIMEOUT = (3.05, 1)
-    TOTAL = 5
-    STATUS_FORCELIST = [413, 429, 500, 502, 503, 504]
-    BACKOFF_FACTOR = 1
+    _timeout = (3.05, 1)
+    _total = 5
+    _status_forcelist = [413, 429, 500, 502, 503, 504]
+    _backoff_factor = 1
 
     def __init__(self, 
-                 timeout: Tuple[float, float] = TIMEOUT, 
-                 total: int = TOTAL, 
-                 status_forcelist: List[int] = STATUS_FORCELIST, 
-                 backoff_factor: int = BACKOFF_FACTOR):
+                 timeout: Tuple[float, float]=_timeout, 
+                 total: int=_total, 
+                 status_forcelist: List[int]=_status_forcelist, 
+                 backoff_factor: int=_backoff_factor):
         self.timeout = timeout
         self.total = total        
         self.status_forcelist = status_forcelist
@@ -68,10 +71,11 @@ class RequestHandler(object):
         })
         return session
     
-    def call_api(self, url: str, params: dict = {}) -> dict:
+    def call_api(self, url: str, params: dict={}) -> dict:
         response = self.session.get(url, timeout = self.timeout, params = params)
         response.encoding = 'utf-8'
         return response.json()
+
 
 class Hentai(RequestHandler):
     HOME = "https://nhentai.net/" 
@@ -80,82 +84,143 @@ class Hentai(RequestHandler):
 
     def __init__(self, 
                  id: int, 
-                 timeout: Tuple[float, float] = RequestHandler.TIMEOUT, 
-                 total: int = RequestHandler.TOTAL, 
-                 status_forcelist: List[int] = RequestHandler.STATUS_FORCELIST, 
-                 backoff_factor: int = RequestHandler.BACKOFF_FACTOR):
+                 timeout: Tuple[float, float]=RequestHandler._timeout, 
+                 total: int=RequestHandler._total, 
+                 status_forcelist: List[int]=RequestHandler._status_forcelist, 
+                 backoff_factor: int=RequestHandler._backoff_factor):
         self.id = id
         super().__init__(timeout, total, status_forcelist, backoff_factor)
         self.handler = RequestHandler(self.timeout, self.total, self.status_forcelist, self.backoff_factor)
         self.url = urljoin(Hentai._URL, str(self.id))
         self.api = urljoin(Hentai._API, str(self.id))
-        self.json = self.handler.call_api(self.api)
+        self.json = self.handler.call_api(self.api) 
     
+    @staticmethod
+    def get_media_id(json: dict) -> int:
+        return int(json['media_id'])
+
     @property
     def media_id(self) -> int:
-        return self.json['media_id']
+        return Hentai.get_media_id(self.json)   
 
-    def title(self, format: Format = Format.English) -> str:
-        return self.json['title'].get(format.value)
+    @staticmethod
+    def get_title(json: dict, format: Format) -> str:
+        return json['title'].get(format.value)
+
+    def title(self, format: Format=Format.English) -> str:
+        return Hentai.get_title(self.json, format)
+
+    @staticmethod
+    def get_cover(json: dict, media_id: int) -> str:
+        cover_ext = Extension.convert(json['images']['cover']['t'])
+        return f"https://t.nhentai.net/galleries/{media_id}/cover.{cover_ext}"
 
     @property
     def cover(self) -> str:
-        cover_ext = Extension.convert(self.json['images']['cover']['t'])
-        return f"https://t.nhentai.net/galleries/{self.media_id}/cover.{cover_ext}"
+        return Hentai.get_cover(self.json, self.media_id)
+
+    @staticmethod
+    def get_thumbnail(json: dict, media_id: int) -> str:
+        thumb_ext = Extension.convert(json['images']['thumbnail']['t'])
+        return f"https://t.nhentai.net/galleries/{media_id}/thumb.{thumb_ext}"
 
     @property
-    def thumbnail(self) -> str:
-        thumb_ext = Extension.convert(self.json['images']['thumbnail']['t'])
-        return f"https://t.nhentai.net/galleries/{self.media_id}/thumb.{thumb_ext}"
+    def thumbnail(self):
+        return Hentai.get_cover(self.json, self.media_id)
+
+    @staticmethod
+    def get_upload_date(json: dict) -> datetime:
+        return datetime.fromtimestamp(json['upload_date'])
 
     @property
     def upload_date(self) -> datetime:
-        return datetime.fromtimestamp(self.json['upload_date'])
+        return Hentai.get_upload_date(self.json)
 
-    _tag = lambda self, type: [Tag(tag['id'], tag['type'], tag['name'], tag['url'], tag['count']) for tag in self.json['tags'] if tag['type'] == type]
+    _tag = lambda json, type: [Tag(tag['id'], tag['type'], tag['name'], tag['url'], tag['count']) for tag in json['tags'] if tag['type'] == type]
     
+    @staticmethod
+    def get_tags(json: dict) -> List[Tag]:
+        return Hentai._tag(json, 'tag')
+
     @property
     def tags(self) -> List[Tag]:
-        return Hentai._tag(self, 'tag')
+        return Hentai.get_tags(self.json)
+
+    @staticmethod
+    def get_language(json: dict) -> List[Tag]:
+        return Hentai._tag(json, 'language')
 
     @property
     def language(self) -> List[Tag]:
-        return Hentai._tag(self, 'language')
+        return Hentai.get_language(self.json)
+
+    @staticmethod
+    def get_artist(json: dict) -> List[Tag]:
+        return Hentai._tag(json, 'artist')
 
     @property
     def artist(self) -> List[Tag]:
-        return Hentai._tag(self, 'artist')
+        return Hentai.get_artist(self.json)
+
+    @staticmethod
+    def get_category(json: dict) -> List[Tag]:
+        return Hentai._tag(json, 'category')
 
     @property
     def category(self) -> List[Tag]:
-        return Hentai._tag(self, 'category')
+        return Hentai.get_category(self.json)
+
+    @staticmethod
+    def get_num_pages(json: dict) -> int:
+        return int(json['num_pages'])
 
     @property
     def num_pages(self) -> int:
-        return self.json['num_pages']
+        return Hentai.get_num_pages(self.json)
+
+    @staticmethod
+    def get_num_favorites(json: dict) -> int:
+        return int(json['num_favorites'])
 
     @property
     def num_favorites(self) -> int:
-        return self.json['num_favorites']
+        return Hentai.get_num_favorites(self.json)
+
+    @staticmethod
+    def get_image_urls(json: dict, media_id: int, num_pages: int) -> List[str]:
+        extension = lambda num: Extension.convert(json['images']['pages'][num]['t'])
+        image_url = lambda num: f"https://i.nhentai.net/galleries/{media_id}/{num}.{extension(num-1)}"
+        return [image_url(num) for num in range(1, num_pages + 1)] 
 
     @property
     def image_urls(self) -> List[str]:
-        extension = lambda num: Extension.convert(self.json['images']['pages'][num]['t'])
-        image_url = lambda num: f"https://i.nhentai.net/galleries/{self.media_id}/{num}.{extension(num-1)}"
-        return [image_url(num) for num in range(1, self.num_pages + 1)] 
+        return Hentai.get_image_urls(self.json, self.media_id, self.num_pages)
 
     @staticmethod
-    def get_random_id(handler = RequestHandler()) -> int:
+    def get_random_id(handler=RequestHandler()) -> int:
         response = handler.session.get(urljoin(Hentai.HOME, 'random'))
         return int(urlparse(response.url).path[3:-1])
 
     @staticmethod
-    def get_homepage(page: int = 1, handler = RequestHandler()) -> List[dict]:
-        response = handler.call_api(urljoin(Hentai.HOME, 'api/galleries/all'), params = { 'page' : page })
+    def browse_homepage(start_page: int, end_page: int, handler=RequestHandler()) -> Iterator[List[dict]]:
+        for page in range(start_page, end_page + 1):
+            payload = { 'page' : page }
+            response = handler.call_api(urljoin(Hentai.HOME, 'api/galleries/all'), params=payload)
+            yield response['result']
+
+    @staticmethod
+    def get_homepage(page: int=1, handler=RequestHandler()) -> List[dict]:
+        return next(Hentai.browse_homepage(page, page, handler))
+
+    @staticmethod
+    def search_by_query(query: str, page: int=1, handler=RequestHandler()) -> List[dict]:
+        payload = { 'query' : query, 'page' : page }
+        response = handler.call_api(urljoin(Hentai.HOME, '/api/galleries/search'), params=payload)
         return response['result']
 
     @staticmethod
-    def search_by_query(query: str, page: int = 1, handler = RequestHandler()) -> List[dict]:
-        payload = { 'query' : query, 'page' : page }
-        response = handler.call_api(urljoin(Hentai.HOME, '/api/galleries/search'), params = payload)
-        return response['result']
+    def search_all_by_query(query: str, handler=RequestHandler()) -> Iterator[List[dict]]:
+        payload = { 'query' : query, 'page' : 1 }
+        response = handler.call_api(urljoin(Hentai.HOME, '/api/galleries/search'), params=payload)
+        for page in range(1, int(response['num_pages']) + 1):
+            yield Hentai.search_by_query(query, page, handler)
