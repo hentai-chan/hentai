d3df8a1ff1c1bdeb4902abc0f66e05fe5b656425
hentaichan <70948244+hentai-chan@users.noreply.github.com>
2020-10-03 21:32:08 +0200

    [TST] Extended test_call_api test scope.

--- tests/test_requesthandler.py
+++ tests/test_requesthandler.py
@@ -4,10 +4,10 @@ from hentai import RequestHandler
 
 
 class TestRequestHandler(unittest.TestCase):
-
     def test_call_api(self):
         response = RequestHandler().call_api(url="https://nhentai.net/api/galleries/all")
         self.assertTrue(response.ok)
+        self.assertNotIn('error', response.json())
         
 
 if __name__ == '__main__':
