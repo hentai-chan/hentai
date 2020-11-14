bf24834bb2fa5a71c8a035fff95fad4425212197
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2020-11-15 00:00:43 +0100

    [MAINT] Reads packages from requirements.txt directly into setup.py.

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ from setuptools import setup
 with open("hentai/__init__.py", encoding='utf8') as file_handler:
     version = re.search(r'__version__ = "(.*?)"', file_handler.read()).group(1)
 
+with open("requirements.txt", encoding='utf-8') as file_handler:
+    packages = [package.split('==')[0] for package in file_handler.readlines()]
+
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="hentai",
     version=version,
-    install_requires=[ "requests", "faker" ],
+    install_requires=packages,
 )
