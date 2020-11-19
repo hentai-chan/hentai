8963487528b1e35a715e9418822f342511df7adc
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2020-11-19 14:28:02 +0100

    [TST] Updates test_download function.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -36,7 +36,7 @@ class TestUtils(unittest.TestCase):
         self.assertTrue(response.ok, msg=f"Failing ID: {random_hentai.id}. Failing URL: {response.url}")
 
     def test_download_queue(self):
-        Utils.download([self.tiny_evil.id])
+        Utils.download([self.tiny_evil])
         self.assertTrue(self.tiny_evil_dir.is_dir())
 
     def test_get_homepage(self):
