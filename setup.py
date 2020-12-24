81c51b0671acb8185f221820c91e512a5c581986
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2020-12-24 12:53:42 +0100

    [ENH] Simplified package extracting.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("hentai/__init__.py", encoding='utf8') as file_handler:
     version = re.search(r'__version__ = "(.*?)"', file_handler.read()).group(1)
 
 with open("requirements.txt", encoding='utf-8') as file_handler:
-    packages = [package.split('==')[0] for package in file_handler.readlines()]
+    packages = file_handler.read().splitlines()
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
