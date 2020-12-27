d2ef21c5aae8ad400e0ae8235ec3b256565b03cb
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2020-12-27 20:34:21 +0100

    [TST] Add test_list case to TestTag class

--- tests/test_tag.py
+++ tests/test_tag.py
@@ -1,6 +1,7 @@
 import unittest
+from urllib.parse import urljoin
 
-from hentai import Hentai, Tag
+from hentai import Hentai, Tag, Option
 
 class TestTag(unittest.TestCase):
     @classmethod
@@ -10,3 +11,16 @@ class TestTag(unittest.TestCase):
     def test_get(self):
         language = self.test_response.language
         self.assertEqual(Tag.get(language, 'name'), "english, translated", msg=f"Language Tag: {language}")
+
+    def test_list(self):
+        characters = Tag.list(Option.Character)
+        for char in characters:
+            if char.name == 'holo':
+                holo = char
+
+                self.assertEqual(holo.id, 33918)
+                self.assertEqual(holo.type, 'character')
+                self.assertEqual(holo.name, 'holo')
+                self.assertEqual(holo.url, urljoin(Hentai.HOME, '/character/holo/'))
+                self.assertEqual(holo.count, 160)
+                break
