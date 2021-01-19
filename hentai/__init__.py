b253634397cc690d07c810aeaa167daa3362f6c9
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-01-19 17:10:18 +0100

    [MAINT] Use regex to parse package name from init

--- hentai/__init__.py
+++ hentai/__init__.py
@@ -1,3 +1,4 @@
 from .hentai import *
 
 __version__ = "3.2.2"
+package_name = "hentai"
--- setup.py
+++ setup.py
@@ -5,14 +5,16 @@ import re
 from setuptools import setup
 
 with open("hentai/__init__.py", encoding='utf8') as file_handler:
-    version = re.search(r'__version__ = "(.*?)"', file_handler.read()).group(1)
+    lines = file_handler.read()
+    version = re.search(r'__version__ = "(.*?)"', lines).group(1)
+    package_name = re.search(r'package_name = "(.*?)"', lines).group(1)
 
 with open("requirements.txt", encoding='utf-8') as file_handler:
     packages = file_handler.read().splitlines()
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-    name="hentai",
+    name=package_name,
     version=version,
     install_requires=packages,
     include_package_data=True
