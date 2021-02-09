2106c03efc03039366a1d8cf40fd2eda64c6d9c9
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-02-09 11:04:02 +0100

    [BUG] Fix version import bug.

--- src/hentai/__init__.py
+++ src/hentai/__init__.py
@@ -3,11 +3,12 @@
 import argparse
 
 from .hentai import *
+from .hentai import __version__
 
 def main():
     parser = argparse.ArgumentParser(prog=package_name)
-    parser.add_argument('--id', type=int, help="download this doujin in the current working directory")
-    parser.add_argument('--version', action='version', version=f"%(prog)s {__version__}")
+    parser.add_argument('-id', type=int, help="download this doujin in the current working directory")
+    parser.add_argument('-version', action='version', version=f"%(prog)s {__version__}")
     args = parser.parse_args()
     Hentai(args.id).download(progressbar=True)
 
